f710d99a874b3867a1fbb58bb77839b72d88d0da
Indifference061 <460541615@qq.com>
2023-10-31 09:46:17 +0800

    remove file

--- lab2/user/freemem.d
+++
@@ -1,2 +0,0 @@
-user/freemem.o: user/freemem.c kernel/types.h kernel/riscv.h \
- kernel/sysinfo.h user/user.h
--- lab2/user/procnum.d
+++
@@ -1,2 +0,0 @@
-user/procnum.o: user/procnum.c kernel/types.h kernel/riscv.h \
- kernel/sysinfo.h user/user.h
